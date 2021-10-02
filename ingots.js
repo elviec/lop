48f5f93f6d7b8cb0f74b3967131928fbae14facf
Sherly Chan <sherlyc@users.noreply.github.com>
2021-10-02 19:40:01 +1300

    Ingots Calculator v2.0

--- ingots.js
+++ ingots.js
@@ -1,59 +1,136 @@
-const daysPerCycle = 24;
-
 function calculate() {
-  const kunningkong = document.querySelector("#kunningkong").value;
+  const kunningkong =
+    parseInt(document.querySelector("#kunningkong").value) | 0;
   const marriage = document.querySelector("#marriage").value;
   const chapter = document.querySelector("#chapter").value;
   const monthly = document.querySelector("#monthly").checked;
   const lifetime = document.querySelector("#lifetime").checked;
-  const labour = document.querySelector("#labour").checked;
-  const building = document.querySelector("#building").value;
+  const labour = document.querySelector("input[name=labour]:checked").value;
+  const building = parseInt(document.querySelector("#building").value);
   const palace = document.querySelector("#palace").checked;
-  const greetingsTotal = kunningkong * daysPerCycle;
-  const marriageTotal = marriage * 20;
-  const chapterTotal = chapter * 100;
-  const worshipTotal = 70 * daysPerCycle;
-  const perksTotal = 400;
-  const weeklySalary = 200;
-  const weeklyShare = 200;
-  const labourTotal = labour ? 30 * daysPerCycle : 0;
-  const monthlyTotal = monthly ? 100 * daysPerCycle : 0;
-  const lifetimeTotal = lifetime ? 120 * daysPerCycle : 0;
-  const dailyBuild = building > 0 ? building * daysPerCycle : 0;
-  const palaceBuild = palace ? getPalaceBuildCost(building) : 0;
+  const guildtoken = document.querySelector(
+    "input[name=guildtoken]:checked"
+  ).value;
+  const edutokens = document.querySelector(
+    "input[name=edutokens]:checked"
+  ).value;
+  const slots = parseInt(document.querySelector("#academyslots").value);
+  const whiteroses = parseInt(
+    document.querySelector("input[name=whiteroses]:checked").value
+  );
+  const beastHuntTurns = parseInt(
+    document.querySelector("#beastHuntTurns").value
+  );
+  const fieldTurns = parseInt(document.querySelector("#fieldTurns").value);
+  const smallBanquet = document.querySelector("#smallBanquet").value;
+  const largeBanquet = document.querySelector("#largeBanquet").value;
 
-  const grandTotal =
-    greetingsTotal +
-    marriageTotal +
-    chapterTotal +
-    worshipTotal +
-    perksTotal +
-    monthlyTotal +
-    lifetimeTotal +
-    labourTotal +
-    weeklySalary +
-    weeklyShare -
-    dailyBuild -
-    palaceBuild;
+  //daily income
+  const worship = 70;
+  const timedLogin = 100;
+  const labourIncome = labour === "1" ? 30 : 15;
+  const monthlyPass = monthly ? 100 : 0;
+  const lifetimePass = lifetime ? 120 : 0;
 
-  print(grandTotal);
+  //dailyExpenses
+  const dailyBuild = building ? building : 0;
+  const dailyGuildTokenCost = guildtoken === "1" ? 100 : 0;
+  const dailyEduTokensCost = edutokens && slots ? slots * edutokens * 10 : 0;
+  const dailyWhiteRosesCost = whiteroses ? whiteroses * 10 : 0;
+  const dailyBeastHuntCost = beastHuntTurns
+    ? getDailyBeastHuntCost(beastHuntTurns)
+    : 0;
+
+  //weekly income
+  const weeklySalary = 50;
+  const weeklyShare = 50;
+  const weeklyPerk = 100;
+
+  //weekly expenses
+  const weeklyFieldCost = fieldTurns > 0 ? getWeeklyFieldCost(fieldTurns) : 0;
+
+  //cycle income
+  const marriageIncome = marriage * 20;
+  const chapterIncome = chapter * 100;
+
+  //cycle expenses
+  const palaceBuild = palace ? getAdditionalPalaceBuildCost(building) : 0;
+  const smallBanquetCost = smallBanquet ? smallBanquet * 100 : 0;
+  const largeBanquetCost = largeBanquet ? largeBanquet * 500 : 0;
+  const banquetTotalCost = smallBanquetCost + largeBanquetCost;
+
+  const dailyIncome =
+    kunningkong +
+    worship +
+    timedLogin +
+    labourIncome +
+    monthlyPass +
+    lifetimePass;
+  const minDailyExpenses =
+    dailyBuild +
+    dailyGuildTokenCost +
+    dailyEduTokensCost +
+    dailyWhiteRosesCost +
+    dailyBeastHuntCost;
+  const weeklyIncome =
+    dailyIncome * 7 + weeklySalary + weeklyShare + weeklyPerk;
+  const minWeeklyExpenses = 7 * minDailyExpenses + weeklyFieldCost;
+  const cycleIncome =
+    weeklyIncome * 3 + dailyIncome * 3 + marriageIncome + chapterIncome;
+  const minCycleExpenses =
+    minWeeklyExpenses * 3 +
+    minDailyExpenses * 3 +
+    palaceBuild +
+    banquetTotalCost;
+
+  const result = {
+    dailyIncome,
+    minDailyExpenses,
+    weeklyIncome,
+    minWeeklyExpenses,
+    cycleIncome,
+    minCycleExpenses,
+  };
+
+  print(result);
+  displayExpenses(
+    dailyEduTokensCost,
+    dailyWhiteRosesCost,
+    dailyBeastHuntCost,
+    weeklyFieldCost,
+    banquetTotalCost
+  );
+
   toggleModal();
 }
 
-function print(grandTotal) {
-  const incomePerCycle = document.querySelector("#incomePerCycle");
-  const averagePerDay = document.querySelector("#averagePerDay");
-  const average30 = document.querySelector("#average30");
-  const average60 = document.querySelector("#average60");
-  const average = parseInt(grandTotal / daysPerCycle);
+function print(result) {
+  const dailyIncomeResult = document.querySelector("#dailyMaxIncome");
+  const dailyMinSpendResult = document.querySelector("#dailyMinSpending");
+  const weeklyIncomeResult = document.querySelector("#weeklyMaxIncome");
+  const weeklyMinSpendResult = document.querySelector("#weeklyMinSpending");
+  const cycleIncomeResult = document.querySelector("#cycleMaxIncome");
+  const cycleMinSpendResult = document.querySelector("#cycleMinSpending");
+  const cycleSaving = document.querySelector("#cycleSaving");
+  const totalSaving = result.cycleIncome - result.minCycleExpenses;
+  const status = document.querySelector("#status");
 
-  incomePerCycle.innerText =
-    grandTotal + " ingots over " + daysPerCycle + " days.";
+  dailyIncomeResult.innerText = result.dailyIncome;
+  dailyMinSpendResult.innerText = result.minDailyExpenses;
+  weeklyIncomeResult.innerText = result.weeklyIncome;
+  weeklyMinSpendResult.innerText = result.minWeeklyExpenses;
+  cycleIncomeResult.innerText = result.cycleIncome;
+  cycleMinSpendResult.innerText = result.minCycleExpenses;
 
-  averagePerDay.innerText =
-    "You earn an average of " + average + " ingots per day";
-  average30.innerText = "Your estimated income over 30 days is " + average * 30;
-  average60.innerText = "Your estimated income over 60 days is " + average * 60;
+  if (totalSaving < 0) {
+    cycleSaving.classList.add("has-text-danger");
+    status.innerText = "Uh oh! You should watch your spending!";
+  } else {
+    cycleSaving.classList.remove("has-text-danger");
+    cycleSaving.classList.add("has-text-success");
+    status.innerText = "You are doing great by having a positive income!";
+  }
+  cycleSaving.innerText = totalSaving;
 }
 
 function toggleModal() {
@@ -61,11 +138,51 @@ function toggleModal() {
   modal.classList.toggle("is-active");
 }
 
-function getPalaceBuildCost(building) {
+function getAdditionalPalaceBuildCost(building) {
   const buildCost = 200 - building;
   if (buildCost > 0) {
     return buildCost * 5;
   } else {
     return 0;
   }
+}
+
+function getWeeklyFieldCost(fieldTurns) {
+  const fieldCost = [10, 10, 20, 20, 40, 40, 80, 80, 160, 160, 320, 320];
+  const arr = fieldCost.slice(0, fieldTurns);
+  const dailyFieldCost = arr.reduce(
+    (previousValue, currentValue) => previousValue + currentValue
+  );
+
+  return dailyFieldCost * 5;
+}
+
+function getDailyBeastHuntCost(beastHuntTurns) {
+  const beastHuntCost = [20, 40, 80, 160, 320];
+  const arr = beastHuntCost.slice(0, beastHuntTurns);
+  const dailyBeastHuntCost = arr.reduce(
+    (previousValue, currentValue) => previousValue + currentValue
+  );
+
+  return dailyBeastHuntCost;
+}
+
+function displayExpenses(
+  dailyEduTokensCost,
+  dailyWhiteRosesCost,
+  dailyBeastHuntCost,
+  weeklyFieldCost,
+  banquetTotalCost
+) {
+  const tokens = document.querySelector("#tokens");
+  const whiteroses = document.querySelector("#whiteroses");
+  const fieldturns = document.querySelector("#fields");
+  const beasthunt = document.querySelector("#beasthunt");
+  const banquetHostingCost = document.querySelector("#banquethostingcost");
+
+  tokens.innerText = dailyEduTokensCost * 7;
+  whiteroses.innerText = dailyWhiteRosesCost * 7;
+  fieldturns.innerText = weeklyFieldCost;
+  beasthunt.innerText = dailyBeastHuntCost * 7;
+  banquetHostingCost.innerText = banquetTotalCost;
 }
